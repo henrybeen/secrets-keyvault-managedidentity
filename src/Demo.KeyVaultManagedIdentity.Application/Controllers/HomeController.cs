d6a1a1366e9848df8085024c9b3807a2aeee713f
Henry Been <consultancy@henrybeen.nl>
2018-09-23 20:57:34 +0200

    Add the code

---
+++ src/Demo.KeyVaultManagedIdentity.Application/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace Demo.KeyVaultManagedIdentity.Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var isManagedIdentity = Environment.GetEnvironmentVariable("MSI_ENDPOINT") != null
+                                    && Environment.GetEnvironmentVariable("MSI_SECRET") != null;
+
+            ViewBag.IsManagedIdentity = isManagedIdentity;
+
+            if (isManagedIdentity)
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var keyVaultClient =
+                    new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var secret =
+                    await keyVaultClient.GetSecretAsync(
+                        "https://demo4847.vault.azure.net/secrets/password");
+
+                ViewBag.Secret = secret.Value;
+            }
+
+            return View();
+        }
+    }
+}
